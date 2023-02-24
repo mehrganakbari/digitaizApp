eb364777e95380467a9ea63747f1bfb430ea30a1
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-02-24 14:51:09 +0330

    navbars completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 import "../common/index.css";
+
+const mobileSearchBtn = document.querySelector(".mobileSearchBtn")
+const mobileSearchBox = document.querySelector(".mobileSearchBox")
+
+mobileSearchBtn.addEventListener("click", () => {
+    mobileSearchBox.classList.toggle("hidden")
+})
