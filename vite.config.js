46789e06b056782dccfabd434ce79d7e3f52363a
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-03-12 11:26:53 +0330

    create new page for Unavailable product

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        unavailableProduct: resolve(__dirname, "unavailableProduct.html"),
       },
     },
   },
