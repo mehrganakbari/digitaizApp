7a55e2d6deea51da5f4d747e2647154b210b7f0b
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-03-21 19:45:23 +0330

    create categorys themplate & cards json

---
+++ src/scripts/category.js
@@ -0,0 +1,24 @@
+const filterMD = document.querySelector(".filterMD");
+
+filterMD.onclick = function () {
+  alert("این دکمه فقط در صفحه اول فعال است");
+};
+
+let brands = [
+  {
+    logo: "/assets/images/img/apple.svg",
+    title: "اپل",
+  },
+  {
+    logo: "/assets/images/img/xiaomi.svg",
+    title: "شیائومی",
+  },
+  {
+    logo: "/assets/images/img/asus.svg",
+    title: "ایسوس",
+  },
+  {
+    logo: "/assets/images/img/lenovo.svg",
+    title: "لنوو",
+  },
+];
