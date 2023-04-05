3a0e4475053f61952c796423bb31bca890d8d9e2
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-04-05 13:57:32 +0430

    create category template

--- src/scripts/category.js
+++ src/scripts/category.js
@@ -4,21 +4,4 @@ filterMD.onclick = function () {
   alert("این دکمه فقط در صفحه اول فعال است");
 };
 
-let brands = [
-  {
-    logo: "/assets/images/img/apple.svg",
-    title: "اپل",
-  },
-  {
-    logo: "/assets/images/img/xiaomi.svg",
-    title: "شیائومی",
-  },
-  {
-    logo: "/assets/images/img/asus.svg",
-    title: "ایسوس",
-  },
-  {
-    logo: "/assets/images/img/lenovo.svg",
-    title: "لنوو",
-  },
-];
+
