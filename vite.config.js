faa43af01a8296a5717715efc52636bcd966f76c
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-04-09 17:17:52 +0430

    sidebar category created

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         category: resolve(__dirname, "category.html"),
+        singleproduct: resolve(__dirname, "singleproduct.html"),
         unavailableProduct: resolve(__dirname, "unavailableProduct.html"),
       },
     },
