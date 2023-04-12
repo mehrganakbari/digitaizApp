9cf5e18fc76c4041f7fcb325a0d51fa128793481
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-04-12 11:29:43 +0430

    add to cart desktop section edieted & mobile add to card created

---
+++ src/scripts/singleproduct.js
@@ -0,0 +1,9 @@
+const addToCartDesktopBtn = document.getElementById("addToCartDesktop");
+const addToCartMobileBtn = document.getElementById("addToCartMobile");
+
+addToCartDesktopBtn.onclick = function () {
+    alert("محصول به سبد خرید شما اضافه شد")
+}
+addToCartMobileBtn.onclick = function () {
+    alert("محصول به سبد خرید شما اضافه شد")
+}
